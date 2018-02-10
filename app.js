ba1f5f485074ab3b9ddb43cf28fec2d18c2f6a82
Juan Montoya <jcmontoya@gmail.com>
2018-02-10 17:18:47 +0000

    Section 1 Lecture 7

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT);
+
+var server = app.listen(app.get('port'), function(){
+    var port = server.address().port;
+   console.log("Magic happens on port " + port); 
+});
