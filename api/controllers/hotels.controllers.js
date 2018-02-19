4d9d7bcd1f072ceef4cb14227d324a546fc53888
Juan Montoya <jcmontoya@gmail.com>
2018-02-19 22:02:12 +0000

    Half way through Section 2 Lecture 22

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -4,43 +4,43 @@ var hotelData = require('../data/hotel-data.json');
 module.exports.hotelsGetAll = function(req, res) {
     
     var db = dbconn.get();
+    var collection = db.collection('hotels');
     
-    console.log("db", db);
-    
-    console.log("GET the hotels");
-    console.log(req.query);
-   
     var offset = 0;
     var count = 5;
    
     if(req.query && req.query.offset){
-       var offset = parseInt(req.query.offset, 10);
+      offset = parseInt(req.query.offset, 10);
     }
     
     if(req.query && req.query.count){
-       var count = parseInt(req.query.count, 10);
+      count = parseInt(req.query.count, 10);
     }
     
-    var returnData = hotelData.slice(offset, offset+count);
-  
-    res
-        .status(200)
-        .json( returnData );
+    collection
+        .find()
+        .skip(offset)
+        .limit(count)
+        .toArray(function(err, docs) {
+            console.log("Found hotels", docs);
+            res
+            .status(200)
+            .json(docs);
+        });
 };
 
 module.exports.hotelsGetOne = function(req, res) {
-    
     var db = dbconn.get();
+    var collection = db.collection('hotels');
+    
     var hotelId = req.params.hotelId;
-    var thisHotel = hotelData[hotelId];
-   console.log("GET hotelId", hotelId);
+    console.log("GET hotelId", hotelId);
     res
         .status(200)
         .json( thisHotel );
 };
 
 module.exports.hotelsAddOne = function(req, res) {
-    
    var db = dbconn.get();
    console.log("POST new hotel");
    console.log(req.body);
