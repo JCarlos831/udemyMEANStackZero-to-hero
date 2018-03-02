ec913a15b167b11f368f8e69720de4141122e916
Juan Montoya <jcmontoya@gmail.com>
2018-03-02 21:29:55 +0000

    Glyphicons not working, tried fontawesome, getting stars to show up, but also displays missing icons after stars

--- public/angular-app/hotel-display/hotel-display-controller.js
+++ public/angular-app/hotel-display/hotel-display-controller.js
@@ -1,9 +1,14 @@
 angular.module('meanhotel').controller('HotelController', HotelController);
 
-function HotelController(hotelDataFactory, $routeParams) {
+function HotelController($routeParams, hotelDataFactory) {
     var vm = this;
     var id = $routeParams.id;
     hotelDataFactory.hotelDisplay(id).then(function(response){
         vm.hotel = response.data;
+        vm.stars = _getStarRating(response.data.stars);
     });
+    
+    function _getStarRating(stars) {
+        return new Array(stars);
+    }
 }
